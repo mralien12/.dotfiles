8a581c59e0b03f8d133126d27d748324f27bf068
Tran Khanh Hoa <trankhanh.hoa@vvdntech.com>
2022-08-25 11:34:06 +0700

    Update files
    Signed-off-by: Tran Khanh Hoa <trankhanh.hoa@vvdntech.com>

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ unlink ~/.bash_aliases_others
 unlink ~/.tmux.conf
 unlink ~/.tmux.conf.local
 unlink ~/.vimrc
+unlink ~/.inputrc
 echo "Unlink dot file done!"
 
 if [ "$1" == "clean" ] ; then
@@ -20,4 +21,5 @@ ln -sfv $current_dir/bash/.bash_aliases_others ~/.bash_aliases_others
 ln -sfv $current_dir/tmux/.tmux.conf ~/.tmux.conf
 ln -sfv $current_dir/tmux/.tmux.conf.local ~/.tmux.conf.local
 ln -sfv $current_dir/vim/.vimrc ~/.vimrc
+ln -sfv $current_dir/inputrc/.inputrc ~/.inputrc
 echo "Create soft link for dotfile done!"
